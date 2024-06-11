8910984c216569c8fb46ad81772249ba5501b830
Sepetech <hruby.b.milan@seznam.cz>
2024-06-11 15:04:32 +0200

    Update 04_uloha.py
    Doplneni kodu

--- 04_uloha.py
+++ 04_uloha.py
@@ -1,1 +1,13 @@
+def odstran_cislo(seznam, cislo):
+    pocet_odstranenych = seznam.count(cislo)
+    for i in range(pocet_odstranenych):
+        seznam.remove(cislo)
+    return pocet_odstranenych
 
+# Ukazkovy seznam
+seznam = [1, 2, 3, 4, 2, 5, 2, 6]
+cislo = 2
+
+pocet_odstranenych = odstran_cislo(seznam, cislo)
+print("Počet odstraněných prvků:", pocet_odstranenych)
+print("Seznam po odstranění čísla", cislo, ":", seznam)
