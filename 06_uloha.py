f8757538898c0118fe39658eb6eb44be6dfdbc8a
Sepetech <hruby.b.milan@seznam.cz>
2024-06-11 15:07:00 +0200

    Update 06_uloha.py
    Doplneni kodu

--- 06_uloha.py
+++ 06_uloha.py
@@ -1,1 +1,12 @@
+def mocniny(seznam, exponent):
+    vysledky = []
+    for cislo in seznam:
+        vysledky.append(cislo ** exponent)
+    return vysledky
+# Ukazkovy seznam
+seznam = [1, 2, 3, 4, 5]
+exponent = 2
 
+novy_seznam = mocniny(seznam, exponent)
+print("Původní seznam:", seznam)
+print("Nový seznam s mocninou", exponent, ":", novy_seznam)
